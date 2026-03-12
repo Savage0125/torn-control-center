71e529cf1304438c409de12cd229e42d035f97c7
Savage0125 <parkerwren0125@gmail.com>
2026-03-12 05:52:16 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const fetch = require("node-fetch")
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const API_KEY = process.env.TORN_API_KEY
+
+app.use(express.static("public"))
+
+app.get("/api/oc", async (req,res)=>{
+
+const url = `https://api.torn.com/v2/faction/crimes?cat=completed&sort=DESC&key=${API_KEY}`
+
+const response = await fetch(url)
+const data = await response.json()
+
+const lastOC = {}
+
+data.crimes.forEach(crime=>{
+
+const time = crime.executed_at
+
+crime.slots.forEach(slot=>{
+
+const id = slot.user_id
+const name = slot.user_name
+
+if(!lastOC[id] || time > lastOC[id].time){
+lastOC[id] = {name,time}
+}
+
+})
+
+})
+
+res.json(lastOC)
+
+})
+
+app.listen(PORT,()=>{
+console.log("Dashboard running")
+})
